97b124b7b7365399054ff4d81fb620c087c5d0f0
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-27 10:51:03 -0500

    have random ball where i want it and has a function that works when i click it

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const balls = document.querySelector(".top-balls");
-//i don't think this is working. i may need to read some documentation.
-TweenMax.to(balls, 2, { x: 100 });
+// const balls = document.querySelector(".top-balls");
+// //i don't think this is working. i may need to read some documentation.
+// TweenMax.to(balls, 2, { x: 100 });
+
+console.log("javascript index works");
+
+function myPlay() {
+  console.log(" clicking random ball works");
+}
