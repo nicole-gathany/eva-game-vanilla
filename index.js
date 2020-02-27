64b3ed53f8b7e2baafeb5552fa757e58c716a618
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-27 11:34:08 -0500

    button changes color of the random ball

--- index.js
+++ index.js
@@ -10,5 +10,15 @@ function myPlay() {
   let colorNum = Math.round(Math.random() * 10);
   console.log(colorNum);
 
-  elem.style.fill = "blue";
+  if (colorNum >= 0 && colorNum < 2) {
+    elem.style.fill = "blue";
+  } else if (colorNum >= 2 && colorNum < 4) {
+    elem.style.fill = "purple";
+  } else if (colorNum >= 4 && colorNum < 6) {
+    elem.style.fill = "red";
+  } else if (colorNum >= 6 && colorNum < 8) {
+    elem.style.fill = "yellow";
+  } else if (colorNum >= 8 && colorNum <= 10) {
+    elem.style.fill = "green";
+  }
 }
