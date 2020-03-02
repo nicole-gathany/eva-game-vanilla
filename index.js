8ff89b73026b94e17df957fe05a6ccb2ef8c67c0
ncgmph2014 <nicole.gathany@gmail.com>
2020-03-01 22:08:19 -0500

    a test that didn't help much

--- index.js
+++ index.js
@@ -49,6 +49,8 @@ function myPlay() {
   let green5 = document.getElementById("green-ball-5");
   let green6 = document.getElementById("green-ball-6");
   let green7 = document.getElementById("green-ball-7");
+  //it looks like it's not reading it at all, so for now i might i have to do this the hard way
+  console.log(green1.style.cx + " is where the green ball 1 is ");
   //not sure about setInterval, may need to look that up
   //what is the handler??
   //didn't realize that the second part of setInterval was in ms. that explains a lot
